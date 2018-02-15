c698f9fad183788d91083ecf425cb8515e492b8d
ThopstadRedner@gmail.com <thopstadredner@gmail.com>
2018-02-14 21:56:44 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter,  Route, Link, Switch } from 'react-router-dom';
 
 import HomePage from './components/HomePage';
 import contactme from './components/contactme';
